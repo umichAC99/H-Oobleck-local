11cc201eda0ac1cd7072789d4f9fb7a22520cd8c
kaplannp <kaplannp@umich.edu>
2024-04-15 22:14:23 -0400

    Restructured for loop for iterating through layer
    Still need to dynamically fix the size of the auto generated input

--- HLOString_Compute/graph2hlo.py
+++ HLOString_Compute/graph2hlo.py
@@ -1,5 +1,5 @@
 import torch
-#import torch.nn as nn
+import torch.nn as nn
 from torch.fx import symbolic_trace
 import tensorflow as tf
 import os
@@ -8,7 +8,9 @@ def graph2hlo(graphModule): #removed the test input cause we need it on the real
     #1) create model.onnx
     # Specify the path to the ONNX file
     onnx_file_path = "model.onnx"
-    dummy_input = (torch.randn(1, 1024), torch.randn(1, 1024), torch.randn(1, 1024))  # fixed this hopefully
+    #dummy_input = (torch.randn(1, 1024), torch.randn(1, 1024), torch.randn(1, 1024))  # fixed this hopefully
+    #TODO need to find this size dynamically by extracting from the graphModule
+    dummy_input = torch.randn(1, 1024)
     torch.onnx.export(graphModule,               # model being run
                       dummy_input,               # model input (or a tuple for multiple inputs)
                       onnx_file_path,            # where to save the model
@@ -33,26 +35,55 @@ def graph2hlo(graphModule): #removed the test input cause we need it on the real
 
     return hloString
 
-# Define a simple neural network module
-class SimpleNN(nn.Module):
-    def __init__(self):
-        super(SimpleNN, self).__init__()
-        self.linear = nn.Linear(10, 5)  # An example layer
+def convertAllLayers(layers):
+    """
+    @param layers: iterable of torch.fx.graphmodule (as found in Oobleck
+      self.model.layers
+    Returns a string delimited by the DELIMITER local variable, separating hlo
+    strings for each layer.
+    @postcondition: writes the string to a file hloOut.txt
+    """
+    allStrings = ""
+    DELIMITER = "\n" + ("="*80) + "\n"
+    for layer in layers:
+        hloString = graph2hlo(layer)
+        allStrings += hloString
+        allStrings += DELIMITER
+    allStrings = allStrings[:-80] #trim the final delimiter
+    with open("hloOut.txt", 'r') as file:
+        file.write(allStrings)
+    return allStrings
 
-    def forward(self, x):
-        x = self.linear(x)
-        return torch.relu(x)
+def main():
+    '''
+    This is a simple main function demonstrates it works for a very basic neural
+    network layer
+    '''
+    # Define a simple neural network module
+    class SimpleNN(nn.Module):
+        def __init__(self):
+            super(SimpleNN, self).__init__()
+            self.linear = nn.Linear(10, 5)  # An example layer
 
-# Create an instance of the
-# module
-simple_nn = SimpleNN()
-# Use torch.fx.symbolic_trace to
-# create a GraphModule
-graph_module = symbolic_trace(simple_nn)
+        def forward(self, x):
+            x = self.linear(x)
+            return torch.relu(x)
 
-# Create a dummy input that matches the input shape the model expects
-dummy_input = torch.randn(1, 10)
+    # Create an instance of the
+    # module
+    simple_nn = SimpleNN()
+    # Use torch.fx.symbolic_trace to
+    # create a GraphModule
+    graph_module = symbolic_trace(simple_nn)
 
-out = graph2hlo(simple_nn, dummy_input)
-with open('out.txt', 'w') as file:
-   file.write(out)
+    
+    breakpoint()
+    # Create a dummy input that matches the input shape the model expects
+    dummy_input = torch.randn(1, 10)
+
+    out = graph2hlo(graph_module)
+    with open('out.txt', 'w') as file:
+       file.write(out)
+
+if __name__ == "__main__":
+    main()
