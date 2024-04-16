b3affb27f236814d8d985794ac79b1a7878f7e35
kaplannp <kaplannp@umich.edu>
2024-04-16 10:32:42 -0400

    Updated and ran on dummy

--- HLOString_Compute/graph2hlo.py
+++ HLOString_Compute/graph2hlo.py
@@ -4,15 +4,22 @@ from torch.fx import symbolic_trace
 import tensorflow as tf
 import os
 
-def graph2hlo(graphModule): #removed the test input cause we need it on the real layers
+def graph2hlo(graphModule, inputWidth, nSamples=1000): #removed the test input cause we need it on the real layers
+    '''
+    input a grahModule, output the HLO string for the module
+    @param graphModule: a pytorch.fx.GraphModule (the layer of Oobleck)
+    @param inputWidth: width of the first layer in the graphModule
+    @param nSamples: default 1000. Batch size/ number of samples to run through
+    @return String: the hlostring
+    '''
     #1) create model.onnx
     # Specify the path to the ONNX file
     onnx_file_path = "model.onnx"
     #dummy_input = (torch.randn(1, 1024), torch.randn(1, 1024), torch.randn(1, 1024))  # fixed this hopefully
     #TODO need to find this size dynamically by extracting from the graphModule
-    dummy_input = torch.randn(1, 1024)
+    testInput = torch.randn(1, inputWidth)
     torch.onnx.export(graphModule,               # model being run
-                      dummy_input,               # model input (or a tuple for multiple inputs)
+                      testInput,               # model input (or a tuple for multiple inputs)
                       onnx_file_path,            # where to save the model
                       export_params=True,        # store the trained parameter weights inside the model file
                       opset_version=11,          # the ONNX version to export the model to
@@ -35,22 +42,23 @@ def graph2hlo(graphModule): #removed the test input cause we need it on the real
 
     return hloString
 
-def convertAllLayers(layers):
+def convertAllLayers(layers, widths):
     """
     @param layers: iterable of torch.fx.graphmodule (as found in Oobleck
       self.model.layers
+    @param widths: iterable of corresponding input widths
     Returns a string delimited by the DELIMITER local variable, separating hlo
     strings for each layer.
     @postcondition: writes the string to a file hloOut.txt
     """
     allStrings = ""
     DELIMITER = "\n" + ("="*80) + "\n"
-    for layer in layers:
-        hloString = graph2hlo(layer)
+    for layer, width in zip(layers, widths):
+        hloString = graph2hlo(layer, width)
         allStrings += hloString
         allStrings += DELIMITER
     allStrings = allStrings[:-80] #trim the final delimiter
-    with open("hloOut.txt", 'r') as file:
+    with open("hloOut.txt", 'w') as file:
         file.write(allStrings)
     return allStrings
 
@@ -72,16 +80,17 @@ def main():
     # Create an instance of the
     # module
     simple_nn = SimpleNN()
+    simple_nn2 = SimpleNN()
     # Use torch.fx.symbolic_trace to
     # create a GraphModule
     graph_module = symbolic_trace(simple_nn)
+    graph_module2 = symbolic_trace(simple_nn)
+    graph_modules = [graph_module, graph_module2]
+    widths = [10, 10]
 
     
-    breakpoint()
-    # Create a dummy input that matches the input shape the model expects
-    dummy_input = torch.randn(1, 10)
-
-    out = graph2hlo(graph_module)
+    out = convertAllLayers(graph_modules, widths)
+    #out = graph2hlo(graph_module)
     with open('out.txt', 'w') as file:
        file.write(out)
 
